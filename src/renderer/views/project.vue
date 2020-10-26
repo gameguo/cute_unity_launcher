e2570fc8ef38fc166b6bdf7750b90f1fb38e9b3e
gameguo <1017619024@qq.com>
2020-10-26 23:57:30 +0800

    table auto height

--- src/renderer/views/project.vue
+++ src/renderer/views/project.vue
@@ -1,20 +1,11 @@
 <template>
   <div class="flexContainer">
     <div class="contentTitle">项目</div>
-    <div class="contentContent" ref="tablewrap">
-      <el-table
-        :data="tableData"
-        :height="tableHeight"
-        id="projectTable"
-        border
-      >
-        <el-table-column prop="date" label="日期" width="180">
-        </el-table-column>
-        <el-table-column prop="name" label="姓名" width="180">
-        </el-table-column>
-        <el-table-column prop="address" label="地址"> </el-table-column>
-      </el-table>
-    </div>
+    <el-table :data="tableData" :height="tableHeight" id="projectTable" border>
+      <el-table-column prop="date" label="日期" width="180"> </el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
+      <el-table-column prop="address" label="地址"> </el-table-column>
+    </el-table>
   </div>
 </template>
 
@@ -23,8 +14,6 @@
 .flexContainer {
   width: 100%;
   height: 100%;
-  display: flex;
-  flex-direction: column;
 }
 .contentTitle {
   height: 40px;
@@ -34,122 +23,46 @@
   line-height: 40px;
   font-size: 20px;
 }
-.contentContent {
-  flex: 1;
-  background-color: coral;
-  margin: 10px;
+#projectTable {
+  padding-right: 20px;
+  margin: 20px;
 }
 </style>
 
 <script>
 export default {
   name: "project",
-  mounted() {
-    this.tableHeight = window.getComputedStyle(this.$refs.tablewrap).height;
-    console.log(
-      window.getComputedStyle(this.$refs.tablewrap).height +
-        " " +
-        this.tableHeight
-    );
-    const that = this;
-    window.onresize = () => {
-      return (() => {
-        window.screenHeight = document.body.clientHeight;
-        that.screenHeight = window.screenHeight;
-      })();
-    };
-  },
-  watch: {
-    screenHeight(val, oldVal) {
-      this.tableHeight = window.getComputedStyle(this.$refs.tablewrap).height;
-      console.log(
-        window.getComputedStyle(this.$refs.tablewrap).height +
-          " " +
-          this.tableHeight
-      );
+  methods: {
+    getHeight: function () {
+      return document.body.clientHeight - 120 + "px";
     },
-  },
-  data() {
-    return {
-      screenHeight: document.body.clientHeight,
-      tableHeight: "",
-      tableData: [
+    setHeight: function () {
+      this.tableHeight = this.getHeight();
+    },
+    getTableData: function () {
+      return [
         {
           date: "2016-05-03",
           name: "王小虎",
           address: "上海市普陀区金沙江路 1518 弄",
         },
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-08",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-06",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-07",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-07",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-07",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-07",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-07",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-07",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-07",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-07",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-07",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-      ],
+      ];
+    },
+  },
+  mounted() {
+    this.setHeight();
+    const that = this;
+    window.onresize = () => {
+      return (() => {
+        window.screenHeight = document.body.clientHeight;
+        this.setHeight();
+      })();
+    };
+  },
+  data() {
+    return {
+      tableHeight: this.getHeight(),
+      tableData: this.getTableData(),
     };
   },
 };
